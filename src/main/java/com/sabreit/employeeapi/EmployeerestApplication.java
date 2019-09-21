3a2bd0fef2fe20abd0641d58c7de4d0211de5418
Saulo Reis <sauloarth@gmail.com>
2019-09-21 11:35:24 -0300

    bootstrapping project

---
+++ src/main/java/com/sabreit/employeeapi/EmployeerestApplication.java
@@ -0,0 +1,13 @@
+package com.sabreit.employeeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeerestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeerestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sabreit/employeeapi/EmployeerestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sabreit.employeeapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmployeerestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
